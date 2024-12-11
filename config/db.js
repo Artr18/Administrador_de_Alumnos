47700a379db350c2d9e380e18e25be964127e0bb
Artr18 <er663699@gmail.com>
2024-12-11 02:12:05 -0600

    Actualizar configuración de conexión a MongoDB Atlas

--- config/db.js
+++ config/db.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
-const MONGO_URI = 'mongodb://localhost:27017/Escuela';
 
-mongoose.connect(MONGO_URI)
+// Obtén la cadena de conexión desde la variable de entorno
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
     .then(() => {
-        console.log('Conectado a MongoBD');
+        console.log('Conectado a MongoDB Atlas');
     })
     .catch((error) => {
-        console.error('Error conectando a MongoBD:', error);
+        console.error('Error conectando a MongoDB Atlas:', error);
     });
--- index.js
+++ index.js
@@ -2,22 +2,28 @@ const express = require('express');
 const Alumno = require('./models/Alumno');
 const methodOverride = require('method-override');
 const app = express();
-require('./config/db');
+require('./config/db'); // Conexión a la base de datos
+
 // Configuración de EJS
 app.set('view engine', 'ejs');
+
 // Middleware para manejar JSON y datos de formularios
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
+
 // Rutas
 const alumnoControllers = require('./controllers/alumnoControllers');
 app.use('/', alumnoControllers);
-// Servir archivos estáticos
+
+// Servir archivos estáticos (si tienes una carpeta 'public')
 app.use(express.static(__dirname + '/public/'));
+
 // Rutas específicas para añadir y modificar alumnos
 app.get('/alumnos/nuevo', (req, res) => {
     res.render('nuevo');
 });
+
 app.get('/alumnos/modificar', async(req, res) => {
     try {
         const alumnos = await Alumno.find();
@@ -27,7 +33,9 @@ app.get('/alumnos/modificar', async(req, res) => {
         res.status(500).json({ error: 'Error de servidor' });
     }
 });
+
 // Iniciar el servidor
-app.listen(3000, function() {
-    console.log('Servidor ejecutándose en el puerto 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
