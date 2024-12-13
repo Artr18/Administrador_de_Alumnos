dc05ea7c0eb2a8b230302f629bde36f36ac95cf3
Antonio <er663699@gmail.com>
2024-12-13 00:43:45 -0600

    Primer commit

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,6 +1,7 @@
 // models/Usuario.js
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+
 
 const usuarioSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
